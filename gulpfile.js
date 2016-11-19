70219d393e3ae311bb878fd6f30da1ffe2e17e67
medleykupps <medleykupps@hotmail.com>
2016-11-19 22:37:03 +1100

    Adding vendor.js and bundle.js gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,47 @@ var sass=require('gulp-sass');
 var inject=require('gulp-inject');
 var wiredep=require('wiredep').stream;
 var del=require('del');
+var jshint=require('gulp-jshint');
+var sourcemaps=require('gulp-sourcemaps');
+var concat=require('gulp-concat');
+var gutil=require('gulp-util');
 
 gulp.task('clean', function(cb) {
     del(['static'], cb);
 });
 
+gulp.task('scripts', ['jshint', 'vendorjs'], function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+            .pipe(concat('bundle.js'))
+            .pipe(gutil.env.type==='production' ? uglify() : gutil.noop())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('static/js/'));
+});
+
+gulp.task('jshint', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+})
+
+gulp.task('vendorjs', function() {
+    return gulp.src([
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/bootstrap_sass/assets/javascripts/bootstrap.js',
+            ])
+        .pipe(sourcemaps.init())
+            .pipe(concat('vendor.js'))
+            .pipe(gutil.env.type==='production' ? uglify() : gutil.noop())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('static/js/'));
+})
+
 gulp.task('styles', function() {
     var injectFiles=gulp.src('src/css/_*.scss', {read:false});
     var transform = function(filepath) { return '@import "' + filepath + '";'; };
     return gulp.src('src/css/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(wiredep())
         .pipe(
             inject(
@@ -37,7 +69,7 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('static/css'));
 });
 
-gulp.task('html', ['styles'], function() {
+gulp.task('html', ['styles','scripts'], function() {
     var injectFiles=gulp.src(['static/css/main.css']);    
     return gulp.src('src/index.html')
         .pipe(inject(injectFiles, {
