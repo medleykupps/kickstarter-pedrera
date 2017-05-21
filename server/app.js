34a5bc1c059e8e95c4a6387a2cb945f96638bc79
Sam Medley <sam.medley@kiandra.com.au>
2017-05-21 15:23:30 +1000

    Setting up some nodejs middleware

--- server/app.js
+++ server/app.js
@@ -6,11 +6,20 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// Initialise middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../build')));
+console.log('Serving files from ' + path.join(__dirname, '../build'));
+
 app.get('/', function(req, res) {
-    res.send("Hello world!  I've changed 2");
+    res.send("Hello world!  I've changed 4");
 })
 
 
 app.listen(8000, function() {
-    logger('listening on 8000');
+    console.log('listening on 8000');
+    console.log(__dirname);
 })
